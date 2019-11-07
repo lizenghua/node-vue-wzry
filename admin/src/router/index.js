010aba49c7141aaeab2604afea5dd18221922252
lizenghua <lizenghuayx@163.com>
2019-11-07 16:23:11 +0800

    解决：在路由中点击了相同的路由（会报不允许vue导航到当前位置）的错误

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -2,12 +2,21 @@
  * @message:
  * @Author: lzh
  * @since: 2019-11-05 11:32:17
- * @lastTime: 2019-11-07 15:59:34
+ * @lastTime: 2019-11-07 16:22:41
  * @LastAuthor: Do not edit
  */
 import Vue from "vue";
 import VueRouter from "vue-router";
 import { routes } from "./routerConfig";
+
+/**
+ * 重写路由的push方法
+ * 解决：在路由中点击了相同的路由（会报不允许vue导航到当前位置）的错误
+ */
+const routerPush = VueRouter.prototype.push;
+VueRouter.prototype.push = function push(location) {
+  return routerPush.call(this, location).catch(error => error);
+};
 
 Vue.use(VueRouter);
 
