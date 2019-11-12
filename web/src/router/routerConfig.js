4d283896ccdae12807149915aca0f2deb037813d
lizenghua <lizenghuayx@163.com>
2019-11-12 16:34:04 +0800

    布局- topbar/nav

--- web/src/router/routerConfig.js
+++ web/src/router/routerConfig.js
@@ -2,13 +2,20 @@
  * @message:
  * @Author: lzh
  * @since: 2019-11-12 14:49:29
- * @lastTime: 2019-11-12 15:08:18
+ * @lastTime: 2019-11-12 15:58:55
  * @LastAuthor: Do not edit
  */
 export const routes = [
   {
     path: "/",
-    name: "Home",
-    component: () => import("@/views/home/home.vue")
+    name: "Main",
+    component: () => import("@/views/main/main.vue"),
+    children: [
+      {
+        path: "/",
+        name: "Home",
+        component: () => import("@/views/home/home.vue")
+      }
+    ]
   }
 ];
--- web/src/views/home/home.vue
+++ web/src/views/home/home.vue
@@ -2,13 +2,12 @@
  * @message: 
  * @Author: lzh
  * @since: 2019-11-12 14:48:44
- * @lastTime: 2019-11-12 15:13:09
+ * @lastTime: 2019-11-12 15:54:55
  * @LastAuthor: Do not edit
  -->
 <template>
   <div class="home">
-    <h3>标题</h3>
-    <p>内容</p>
+    <h3>首页</h3>
   </div>
 </template>
 
